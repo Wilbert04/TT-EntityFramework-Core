25c6479fcd414bc47c5e62080fa229a2ca2c8685
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-02-12 21:32:07 -0600

    Ejemplo DBContext- SchoolContext creado

--- Toturial-EntityFrameworkC/DAL/SchoolContext.cs
+++ Toturial-EntityFrameworkC/DAL/SchoolContext.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Toturial_EntityFrameworkC.DAL
 {
-    
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Person> Person { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = SchoolDB; Trusted_Connection = True; ");
+        }
+
+        //Example of modelBuilder
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //With this configurations I force to colocate the primary key manually "Just to Person's entity"
+            modelBuilder.Entity<Person>().Property(p => p.personId).HasColumnName("Id").HasDefaultValue(0).IsRequired();
+
+            //shadow property
+            modelBuilder.Entity<Person>().Property<String>("Ocupacion");
+            modelBuilder.Entity<Person>().Property<int>("Edad");
+        }
+
+
+    }
 }
